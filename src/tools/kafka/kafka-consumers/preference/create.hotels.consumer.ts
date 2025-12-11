4cc17e4de0f6ec776ca8e29b25f4ea97c9c890e8
MD Shariful Islam <79306902+mdsharifulislam-r@users.noreply.github.com>
2025-12-11 22:01:08 +0600

    fix the error

--- src/tools/kafka/kafka-consumers/preference/create.hotels.consumer.ts
+++ src/tools/kafka/kafka-consumers/preference/create.hotels.consumer.ts
@@ -21,55 +21,61 @@ export const addHotelsInPreference = async () => {
       userAddress: { city: string; country: string };
     }) => {
 
-        
-    //   const cityInfo = await AiHelper.getCityInfo(
-    //     userAddress.city,
-    //     userAddress.country
-    //   );
+
+      //   const cityInfo = await AiHelper.getCityInfo(
+      //     userAddress.city,
+      //     userAddress.country
+      //   );
 
 
-      
-
-    //   const arr = {
-    //     hotels: [] as HotelsResponse['data'],
-    //     flights: [] as FlightOfferPricingResponse['data']['flightOffers'],
-    //     activities: [] as ActivitiesResponse['data'],
-    //   };
-
-    //   const data = await Promise.all(
-    //     cityInfo?.suggestCountrys?.map(async country => {
-    //       const hotels = await amaduesHelper.getHotelsList(country.city);
-
-    //       return hotels?.data?.slice(0, 4);
-    //     })
-    //   );
-
-    //   await Preference.findOneAndUpdate({ user: userId }, {hotels:data.flat()});
 
 
-    //   await Promise.all([
-    //     kafkaProducer.sendMessage('activity-in-preference', { userId,userAddress:cityInfo }),
-    //   ])
+      //   const arr = {
+      //     hotels: [] as HotelsResponse['data'],
+      //     flights: [] as FlightOfferPricingResponse['data']['flightOffers'],
+      //     activities: [] as ActivitiesResponse['data'],
+      //   };
 
-    const data:IHomeItem[] = await AiHelper.createAiSuggestionTravelDestination(userId,{city:userAddress.city,country:userAddress.country});
+      //   const data = await Promise.all(
+      //     cityInfo?.suggestCountrys?.map(async country => {
+      //       const hotels = await amaduesHelper.getHotelsList(country.city);
 
-    const mapItems = await Promise.all(
-      data?.map(async (item) => {
-        const images = await getImagesFromApi(item.name);
-        return { ...item, images, user: userId,location:{
-            type:"Point",
-            coordinates:[item.lng,item.lat]
-        }};
-      })
-    );
+      //       return hotels?.data?.slice(0, 4);
+      //     })
+      //   );
+
+      //   await Preference.findOneAndUpdate({ user: userId }, {hotels:data.flat()});
 
 
-    await HomeItem.deleteMany({ user: userId });
+      //   await Promise.all([
+      //     kafkaProducer.sendMessage('activity-in-preference', { userId,userAddress:cityInfo }),
+      //   ])
+      try {
 
-    
-    await HomeItem.insertMany(mapItems)
+      } catch (error) {
+        const data: IHomeItem[] = await AiHelper.createAiSuggestionTravelDestination(userId, { city: userAddress.city, country: userAddress.country });
 
-    
+        const mapItems = await Promise.all(
+          data?.map(async (item) => {
+            const images = await getImagesFromApi(item.name);
+            return {
+              ...item, images, user: userId, location: {
+                type: "Point",
+                coordinates: [item.lng, item.lat]
+              }
+            };
+          })
+        );
+
+
+        await HomeItem.deleteMany({ user: userId });
+
+
+        await HomeItem.insertMany(mapItems)
+      }
+
+
+
     },
   });
 };
